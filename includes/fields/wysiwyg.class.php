905061f36ce0db32316caa28eb4cb0786cf92d45
Craig Kuhns <craigkuhns@gmail.com>
2012-12-02 08:41:27 -0500

    Dirty fix for problem of wysiwyg fields on taxonomy forms having quotes escaped.

--- includes/fields/wysiwyg.class.php
+++ includes/fields/wysiwyg.class.php
@@ -8,6 +8,12 @@ class ForemanWysiwygField extends ForemanField {
     $this->taxonomy_template_path = foreman_template_path('fields/taxonomy/wysiwyg.php');
     if (isset($args['options'])) $this->options = $args['options'];
   }
+
+  // TODO: Find out root cause of slashes being added to taxonomy pages and fix it to get rid of
+  // need for double stripslashes hack.
+  function save_for_taxonomy($new, $old, $term_id) {
+    if (($new != '') && ($new != $old)) {
+      foreman_update_tax_meta($term_id, $this->id, stripslashes(stripslashes($new)));
+    }
+  }
 }
-
-
